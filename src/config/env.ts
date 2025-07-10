a9dab34b16b1b13718434abb2b27aa4f24eb3942
sabbirziauddin <sabbircseiiuc@gmail.com>
2025-07-10 15:57:31 +0200

    folder structure

---
+++ src/config/env.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+interface IEnvVars {
+    port: string | number;
+    dbUrl: string;
+    nodeEnv: string;
+}
+
+const loadEnvVars = (): IEnvVars => {
+    const requiredEnvVars:string[] = [ "PORT", "DB_URL", "NODE_ENV" ];
+    requiredEnvVars.forEach((envVar) => {
+        if (!process.env[envVar]) {
+            throw new Error(`Environment variable ${envVar} is not set`);
+        }
+    });
+
+    return {
+        port: process.env.PORT || 4000,
+        dbUrl: process.env.DB_URL || "mongodb://localhost:27017/TourBackendDB",
+        nodeEnv: process.env.NODE_ENV || "development",
+    };
+
+}
+
+export const envVars = loadEnvVars();
+ 
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,68 @@
-import { Request, Response } from "express";
 import app from "./app";
+import { envVars } from "./config/env";
 
-const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+let server: any;
 
-const port = 4000;
-
-const startServer = async () =>{
-    try {
-        await mongoose.connect(
-          "mongodb+srv://mongodb:mongodb@cluster0.uqid44n.mongodb.net/TourBackendDB?retryWrites=true&w=majority&appName=Cluster0"
-        );
-        console.log("database connected successfully");
-        app.listen (port, ()=>{
-            console.log(`server is running on port ${port}`);
-        })
-        
-    } catch (error) {
-        console.log (error);
-        
-    }
- 
-}
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.dbUrl);
+    console.log("database connected successfully");
+    server = app.listen(envVars.port, () => {
+      console.log(`server is running on port ${envVars.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 startServer();
+//unhandle exeption
+process.on("uncaughtException", (error) => {
+  if (server) {
+    console.log("Uncaught Exception: ", error);
+    process.exit(1); // Exit the process with failure
+  }
+  process.exit(1); // Exit the process with success
+});
+
+//Promise.reject(new Error("I forgot to catch this uncaught exception"));
+process.on("unhandledRejection", (error) => {
+  if (server) {
+    console.log("uncaught rejection", error);
+    process.exit(1);
+  }
+  process.exit(1);
+});
+
+//throw new Error("forgot to caught unhandle rejection");
+
+//handle sigterm
+/* process.on("SIGTERM", () => {
+  if (server) {
+    console.log("SIGTERM received, shutting down gracefully");
+    server.close(() => {
+      console.log("Process terminated");
+    });
+  }
+}); */
+//handle SIGINT
+/* process.on("SIGINT", () => {
+  if (server) {
+    console.log("SIGINT received, shutting down gracefully");
+    server.close(() => {
+      console.log("Process terminated");
+    });
+  }
+}); */
+//check sigterm
+/* process.on("SIGUSR2", () => {
+  if (server) {
+    console.log("SIGUSR2 received, shutting down gracefully");
+    server.close(() => {
+      console.log("Process terminated");
+    });
+  }
+}); */
+//check SIGUSR2
